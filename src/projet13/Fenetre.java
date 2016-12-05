5ea47cd7a49448072642c183f06bcdad95a9bbe6
nicrausaz <n.crausaz99@gmail.com>
2016-12-05 15:24:27 +0100

    ajout barre d'outils

--- src/projet13/Fenetre.java
+++ src/projet13/Fenetre.java
@@ -13,12 +13,13 @@ public class Fenetre extends JFrame {
     JPanel container = new JPanel();
 
     JMenuBar menubar = new JMenuBar();
+    JToolBar toolBar = new JToolBar();
 
     JMenu menuCouleur = new JMenu("Couleur");
     JMenu menuFormes = new JMenu("Formes");
     JMenu menuDimensions = new JMenu("Dimensions");
 
-    JMenuItem itemBlanc = new JMenuItem("Blanc");
+    JMenuItem itemBlanc = new JMenuItem(new ImageIcon("icons/blanc.gif") + "Blanc");
     JMenuItem itemRouge = new JMenuItem("Rouge");
     JMenuItem itemVert = new JMenuItem("Vert");
     JMenuItem itemJaune = new JMenuItem("Jaune");
@@ -31,12 +32,19 @@ public class Fenetre extends JFrame {
     JMenuItem itemLargeur = new JMenuItem("Largeur");
     JMenuItem itemHauteur = new JMenuItem("Hauteur");
 
+    JButton boutonBlanc = new JButton(new ImageIcon("icons/blanc.gif"));
+    JButton boutonRouge = new JButton(new ImageIcon("icons/rouge.gif"));
+    JButton boutonVert = new JButton(new ImageIcon("icons/vert.gif"));
+    JButton boutonJaune = new JButton(new ImageIcon("icons/jaune.gif"));
+    JButton boutonBleu = new JButton(new ImageIcon("icons/bleu.gif"));
+
     public Fenetre() {
         this.setSize(widthScreen / 2, heightScreen / 2);
         this.setTitle("Figures avec menu et barre d'outils");
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(container);
+        container.setLayout(new BorderLayout());
 
         //ajout des items, menus et barre de menus
         menuCouleur.add(itemBlanc);
@@ -52,8 +60,15 @@ public class Fenetre extends JFrame {
         menubar.add(menuCouleur);
         menubar.add(menuFormes);
         menubar.add(menuDimensions);
-
         this.setJMenuBar(menubar);
+
+        //ajouts barre d'outils
+        toolBar.add(boutonBlanc);
+        toolBar.add(boutonRouge);
+        toolBar.add(boutonVert);
+        toolBar.add(boutonJaune);
+        toolBar.add(boutonBleu);
+        container.add(toolBar, BorderLayout.NORTH);
 
         this.setVisible(true);
     }
