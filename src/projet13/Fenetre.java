532d5026e11d7b4845e3f1b0c6dbb4272afaf0c1
nicrausaz <n.crausaz99@gmail.com>
2016-12-06 17:53:13 +0100

    big refactor

--- src/projet13/Fenetre.java
+++ src/projet13/Fenetre.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Fenetre extends JFrame implements ActionListener {
+public class Fenetre extends JFrame {
 
     Toolkit tk = Toolkit.getDefaultToolkit();
     Dimension dimScreen = tk.getScreenSize();
@@ -13,31 +13,8 @@ public class Fenetre extends JFrame implements ActionListener {
 
     JPanel container = new JPanel();
 
-    JMenuBar menubar = new JMenuBar();
-    JToolBar toolBar = new JToolBar();
-
-    JMenu menuCouleur = new JMenu("Couleur");
-    JMenu menuFormes = new JMenu("Formes");
-    JMenu menuDimensions = new JMenu("Dimensions");
-
-    JMenuItem itemBlanc = new JMenuItem(new ImageIcon("icons/blanc.gif") + "Blanc");
-    JMenuItem itemRouge = new JMenuItem("Rouge");
-    JMenuItem itemVert = new JMenuItem("Vert");
-    JMenuItem itemJaune = new JMenuItem("Jaune");
-    JMenuItem itemBleu = new JMenuItem("Bleu");
-
-    JMenuItem itemRectangle = new JCheckBoxMenuItem("Rectangle");
-    JMenuItem itemOvale = new JCheckBoxMenuItem("Ovale");
-    JMenuItem itemTriangle = new JCheckBoxMenuItem("Triangle");
-
-    JMenuItem itemLargeur = new JMenuItem("Largeur");
-    JMenuItem itemHauteur = new JMenuItem("Hauteur");
-
-    JButton boutonBlanc = new JButton(new ImageIcon("icons/blanc.gif"));
-    JButton boutonRouge = new JButton(new ImageIcon("icons/rouge.gif"));
-    JButton boutonVert = new JButton(new ImageIcon("icons/vert.gif"));
-    JButton boutonJaune = new JButton(new ImageIcon("icons/jaune.gif"));
-    JButton boutonBleu = new JButton(new ImageIcon("icons/bleu.gif"));
+    Menubar menubar = new Menubar();
+    Toolbar toolBar = new Toolbar();
 
     public Fenetre() {
         this.setSize(widthScreen / 2, heightScreen / 2);
@@ -47,70 +24,12 @@ public class Fenetre extends JFrame implements ActionListener {
         this.setContentPane(container);
         container.setLayout(new BorderLayout());
 
-        //ajout des items, menus et barre de menus
-        menuCouleur.add(itemBlanc);
-        menuCouleur.add(itemRouge);
-        menuCouleur.add(itemVert);
-        menuCouleur.add(itemJaune);
-        menuCouleur.add(itemBleu);
-        menuFormes.add(itemRectangle);
-        menuFormes.add(itemOvale);
-        menuFormes.add(itemTriangle);
-        menuDimensions.add(itemLargeur);
-        menuDimensions.add(itemHauteur);
-        menubar.add(menuCouleur);
-        menubar.add(menuFormes);
-        menubar.add(menuDimensions);
+        //ajout barre de menus
         this.setJMenuBar(menubar);
 
         //ajouts barre d'outils
-        toolBar.add(boutonBlanc);
-        toolBar.add(boutonRouge);
-        toolBar.add(boutonVert);
-        toolBar.add(boutonJaune);
-        toolBar.add(boutonBleu);
         container.add(toolBar, BorderLayout.NORTH);
 
-        //ajouts des accélérateurs
-        itemRectangle.setMnemonic('R');
-        itemRectangle.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.CTRL_MASK));
-        itemOvale.setMnemonic('O');
-        itemOvale.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
-        itemTriangle.setMnemonic('T');
-        itemTriangle.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, InputEvent.CTRL_MASK));
-        itemLargeur.setMnemonic('L');
-        itemLargeur.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_MASK));
-        itemHauteur.setMnemonic('H');
-        itemHauteur.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, InputEvent.CTRL_MASK));
-
-        //ajouts des infosbulles
-        itemBlanc.setToolTipText("Fond Blanc");
-        itemRouge.setToolTipText("Fond Rouge");
-        itemVert.setToolTipText("Fond Vert");
-        itemJaune.setToolTipText("Fond Jaune");
-        itemBleu.setToolTipText("Fond Bleu");
-        boutonBlanc.setToolTipText("Fond Blanc");
-        boutonRouge.setToolTipText("Fond Rouge");
-        boutonVert.setToolTipText("Fond Vert");
-        boutonJaune.setToolTipText("Fond Jaune");
-        boutonBleu.setToolTipText("Fond Bleu");
-        itemRectangle.setToolTipText("Dessine un rectangle");
-        itemOvale.setToolTipText("Dessine un ovale");
-        itemTriangle.setToolTipText("Dessine un triangle");
-        itemLargeur.setToolTipText("Modifie la largeur");
-        itemHauteur.setToolTipText("Modifie la hauteur");
-
-        //ajouts des listeners
-        itemLargeur.addActionListener(this);
-
         this.setVisible(true);
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == itemLargeur) {
-            FenDialog largeurDialog = new FenDialog();
-            largeurDialog.setTitle("Choisir Largeurs");
-        }
-
     }
 }
